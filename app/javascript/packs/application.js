4cbe8ffbd2d3dc67be30db7a5200a589b190712a
Philipp Moeller <philipp_moeller@hotmail.de>
2020-08-12 08:48:36 +0200

    improved homepage view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -35,3 +35,15 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
   initMapbox();
 });
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".navbar-home").style.top = "0";
+  } else {
+    document.querySelector(".navbar-home").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
