acbf317eb1e1f215488551f1b9550d24b349d490
Luissa Schemuth <luissa.schemuth@outlook.com>
2020-08-12 16:19:51 +0200

    correct price on modal and add to cart link

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,21 @@
 class OrdersController < ApplicationController
+
+  def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @basket = Basket.find_by(user_id: current_user.id)
+    @order = Order.new
+  end
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @basket = Basket.find_by(user_id: current_user.id)
+    @order = Order.new()
+    @order.basket = @basket
+    @order.recipe = @recipe
+    if @order.save
+      redirect_to recipes_path, notice: 'Recipe was successfully added to your basket.'
+    else
+      render :new, notice: 'Recipe could not be added to your basket.'
+    end
+  end
 end
