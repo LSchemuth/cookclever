ac334d0aab70c8b5f53ffd77fa54a0bf8aa33d6e
Philipp Moeller <philipp_moeller@hotmail.de>
2020-08-17 16:08:21 +0200

    Food API

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,6 +26,76 @@ require 'json'
 # file = URI.open('https://res.cloudinary.com/dovh6vnvn/image/upload/v1597241663/Download_1_twljdp.jpg')
 # recipe.photo.attach(io: file, filename: recipe.title, content_type: 'image/jpg')
 
+# puts "Cleaning database..."
+# Amount.destroy_all
+# Ingredient.destroy_all
+# Order.destroy_all
+# Recipe.destroy_all
+# Supermarket.destroy_all
+# Basket.destroy_all
+# User.destroy_all
+
+# puts "Creating user"
+# user = User.create(email: "lschemuth@hotmail.de", password: "111111")
+
+# puts 'Creating 56 fake supermarkets...'
+# counter = 0
+# addresses_array = []
+# addresses = JSON.parse(File.read(Rails.root.join('addresses.json')))
+# # addresses = JSON.parse(File.read'/home/pmoeller/code/LSchemuth/cookclever/addresses.json')
+# addresses.each do |address|
+#   addresses_array << address["address"]
+# end
+
+# supermarket_name = [ "Rewe", "Lidl", "Edeka", "Albert Heijn", "Carrefour", "Aldi", "Tesco", "Sainsbury's" ]
+
+# 15.times do
+#   supermarket = Supermarket.new(
+#     name: supermarket_name.sample,
+#     address: addresses_array[counter],
+#     # address: addresses[counter],
+#     user: user
+#     )
+#   counter += 1
+
+#   supermarket.save!
+
+#   recipes = []
+#   rand(2..7).times do
+#     recipe = Recipe.new(
+#       title:  Faker::Food.dish,
+#       description: Faker::Food.description,
+#       supermarket_id: supermarket.id,
+#       )
+#     file = URI.open("https://images.unsplash.com/photo-1563379926898-05f4575a45d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80")
+#     recipe.photo.attach(io: file, filename: "#{recipe.title}.png", content_type: 'image/png')
+#     recipe.save!
+#     recipes << recipe
+# end
+
+#   recipes.each do |recipe|
+#     rand(2..10).times do
+#       ingredient = Ingredient.create(
+#         name:  Faker::Food.ingredient,
+#         price:  rand(1..4),
+#         # price: Faker::Number.decimal(l_digits= 2, r_digits= 2).within(range: 0..4),
+#         expiration_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
+#         supermarket_id: supermarket.id
+#         )
+#       amount = Amount.create(
+#         quantity: rand(1..5),
+#         unit: Faker::Food.metric_measurement,
+#         recipe_id: recipe.id,
+#         ingredient_id: ingredient.id
+#       )
+#     end
+#   end
+# puts "finished one supermarket"
+# end
+
+
+# puts 'Finished!'
+
 puts "Cleaning database..."
 Amount.destroy_all
 Ingredient.destroy_all
@@ -35,10 +105,15 @@ Supermarket.destroy_all
 Basket.destroy_all
 User.destroy_all
 
+
+require 'uri'
+require 'net/http'
+require 'openssl'
+
 puts "Creating user"
 user = User.create(email: "lschemuth@hotmail.de", password: "111111")
 
-puts 'Creating 56 fake supermarkets...'
+puts 'Creating 15 fake supermarkets...'
 counter = 0
 addresses_array = []
 addresses = JSON.parse(File.read(Rails.root.join('addresses.json')))
@@ -49,8 +124,8 @@ end
 
 supermarket_name = [ "Rewe", "Lidl", "Edeka", "Albert Heijn", "Carrefour", "Aldi", "Tesco", "Sainsbury's" ]
 
-15.times do
-  supermarket = Supermarket.new(
+5.times do
+  p supermarket = Supermarket.new(
     name: supermarket_name.sample,
     address: addresses_array[counter],
     # address: addresses[counter],
@@ -61,90 +136,58 @@ supermarket_name = [ "Rewe", "Lidl", "Edeka", "Albert Heijn", "Carrefour", "Aldi
   supermarket.save!
 
   recipes = []
-  rand(2..7).times do
-    recipe = Recipe.new(
-      title:  Faker::Food.dish,
-      description: Faker::Food.description,
-      supermarket_id: supermarket.id,
-      )
-    file = URI.open("https://images.unsplash.com/photo-1563379926898-05f4575a45d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80")
-    recipe.photo.attach(io: file, filename: "#{recipe.title}.png", content_type: 'image/png')
-    recipe.save!
-    recipes << recipe
-end
+  3.times do
+    # API CALL
+    begin
+      url = URI("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random")
+      
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      
+      request = Net::HTTP::Get.new(url)
+      request["x-rapidapi-host"] = 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+      request["x-rapidapi-key"] = '79df08cf1amsh2662634d1ced4d2p1f8ed2jsn710d56080115'
+      
+      response = http.request(request)
+      api_answer = JSON.parse response.read_body
+      api_recipe = api_answer["recipes"].first
+      binding.pry
+    rescue => exception
+      p exception
+    end
 
-  recipes.each do |recipe|
-    rand(2..10).times do
-      ingredient = Ingredient.create(
-        name:  Faker::Food.ingredient,
-        price:  rand(1..4),
-        # price: Faker::Number.decimal(l_digits= 2, r_digits= 2).within(range: 0..4),
-        expiration_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
-        supermarket_id: supermarket.id
-        )
-      amount = Amount.create(
-        quantity: rand(1..5),
-        unit: Faker::Food.metric_measurement,
-        recipe_id: recipe.id,
-        ingredient_id: ingredient.id
+    # Recipe creation
+    p recipe = Recipe.new(
+      title:  api_recipe['title'],
+      description: api_recipe['instructions'],
+      supermarket: supermarket,
       )
+    file = URI.open(api_recipe['image'])
+    type = api_recipe['imageType']
+    recipe.photo.attach(io: file, filename: "#{recipe.title}.#{type}", content_type: "image/#{type}")
+    recipe.save!
+   
+
+    # Ingredients creation for this recipe
+    api_recipe["extendedIngredients"].each do |api_ingredient|
+        p ingredient = Ingredient.create!(
+          name:  api_ingredient["name"],
+          price:  rand(1..4),
+          expiration_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
+          supermarket: supermarket
+          )
+
+        amount = Amount.create!(
+          quantity: api_ingredient["amount"],
+          unit: api_ingredient["unit"],
+          recipe: recipe,
+          ingredient: ingredient
+        )
     end
   end
-puts "finished one supermarket"
+  puts "finished a supermarket"
 end
 
 
 puts 'Finished!'
-
-
-
-    # -----------
-
-    # puts 'Creating 100 fake restaurants...'
-    # 100.times do
-    #   restaurant = Restaurant.new(
-        #     name:    Faker::Company.name,
-        #     address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
-        #     rating:  rand(0..5)
-        #   )
-        #   restaurant.save!
-        # end
-        # puts 'Finished!'
-
-        # # -----------
-
-        # puts 'Creating 5 products...'
-        # 5.times do |i|
-        #   product = Product.create!(
-            #     name: Faker::Company.name,
-            #     tagline: Faker::Company.catch_phrase
-            #   )
-            #   puts "#{i + 1}. #{product.name}"
-            # end
-            # puts 'Finished!'
-
-            # require "faker"
-            # # TODO: Write a seed to insert 100 posts in the database
-            # require 'faker'
-            # 100.times do
-            #   Post.create(title: Faker::Hacker.say_something_smart, url: Faker::Internet.url,
-            #               votes: Faker::Number.within(range: 1..10_000_000))
-            # end
-
-            # # -----------
-            # require 'json'
-
-            # puts "Cleaning database..."
-            # Ingredient.destroy_all
-
-
-            # url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-
-          # ingredient_data = open(url).read
-          # ingredients = JSON.parse(ingredient_data)
-
-          # ingredients["drinks"].each do |ingredient|
-          #   Ingredient.create(name: ingredient["strIngredient1"])
-          # end
-
-          # puts "Finished!"
