a3805f7e216ee37ca04ce3570fef17424d73207c
Philipp Moeller <philipp_moeller@hotmail.de>
2020-08-17 20:09:49 +0200

    navbar disappears when scrolling down

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,15 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbarHome").style.top = "0";
+  } else {
+    document.getElementById("navbarHome").style.top = "-120px";
+  }
+  prevScrollpos = currentScrollPos;
+}
