7208c3b2400c900f70f2305bec9b45298b201c10
Luissa Schemuth <luissa.schemuth@outlook.com>
2020-08-21 14:51:14 +0200

    mailer?

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
   def welcome
     @user = params[:user]
     p @user
-    mail(to: @user.email, subject: "Your order details")
+    mail(to: 'lotta-schwarz@outlook.com', subject: "Your order details")
   end
 end
